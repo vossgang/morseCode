09ed850be8bb7603853ba9126f62ba222ef5bc8b
vossgang <vossgang@gmail.com>
2014-04-14 11:25:12 -0700

    Initial Commit

---
+++ Morse Code/MVViewController.h
@@ -0,0 +1,13 @@
+//
+//  MVViewController.h
+//  Morse Code
+//
+//  Created by Matthew Voss on 4/14/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVViewController : UIViewController
+
+@end
---
+++ Morse Code/MVViewController.m
@@ -0,0 +1,29 @@
+//
+//  MVViewController.m
+//  Morse Code
+//
+//  Created by Matthew Voss on 4/14/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import "MVViewController.h"
+
+@interface MVViewController ()
+
+@end
+
+@implementation MVViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
