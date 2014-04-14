38304ee480b15bda1af1aab4732bdbcf6cc97bb1
vossgang <vossgang@gmail.com>
2014-04-14 14:38:11 -0700

    Auto layout working

--- Morse Code/MVViewController.m
+++ Morse Code/MVViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "MVViewController.h"
+#import "NSString+MorseCodeString.h"
 
-@interface MVViewController ()
+@interface MVViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *textEntryBox;
+@property (weak, nonatomic) IBOutlet UILabel *translationLabel;
 
 @end
 
@@ -17,6 +21,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    _imageView.backgroundColor = [UIColor lightGrayColor];
+    
+    _translationLabel.text = @" ";
+    
+    _textEntryBox.delegate = self;
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +36,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)translateToMorse:(id)sender
+{
+    
+    _translationLabel.text = [NSString convertStringToMorse:_textEntryBox.text];
+    
+    _imageView.backgroundColor = [self randomColor];
+    
+}
+
+-(UIColor *)randomColor
+{
+    CGFloat r = arc4random_uniform(101) / (float)100;
+    CGFloat g = arc4random_uniform(101) / (float)100;
+    CGFloat b = arc4random_uniform(101) / (float)100;
+    
+    return [UIColor colorWithRed:r green:g blue:b alpha:1];
+    
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
 
 @end
