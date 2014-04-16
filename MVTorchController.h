c69fec69ef3fc59c6d0c396f72ae8a75dcdddf7f
vossgang <vossgang@gmail.com>
2014-04-16 13:02:36 -0700

    happy

---
+++ MVTorchController.h
@@ -0,0 +1,13 @@
+//
+//  MVTorchController.h
+//  Morse Code
+//
+//  Created by Matthew Voss on 4/16/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MVTorchController : NSObject
+
+@end
---
+++ MVTorchController.m
@@ -0,0 +1,13 @@
+//
+//  MVTorchController.m
+//  Morse Code
+//
+//  Created by Matthew Voss on 4/16/14.
+//  Copyright (c) 2014 Matthew Voss. All rights reserved.
+//
+
+#import "MVTorchController.h"
+
+@implementation MVTorchController
+
+@end
